cc899641bebee78c78745c565b3c8f4714f5ebfc
Harshsinha00 <55646952+Harshsinha00@users.noreply.github.com>
2023-05-23 13:20:18 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,6 @@
+var items= document.getElementsByClassName('list-group-item');
+items[3].style.backgroundColor = 'green'
+for(var i=0; i < items.length; i++)
+{
+ items[i].style.fontweight = 'bold';
+}
