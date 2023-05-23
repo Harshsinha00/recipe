ff288659a5b8529f73ecaff28fc353a3abb75392
Harshsinha00 <55646952+Harshsinha00@users.noreply.github.com>
2023-05-23 13:28:51 +0530

    tagname dom.js

--- dom.js
+++ dom.js
@@ -1,6 +1,13 @@
-var items= document.getElementsByClassName('list-group-item');
-items[3].style.backgroundColor = 'green'
-for(var i=0; i < items.length; i++)
+//var items= document.getElementsByClassName('list-group-item');
+//items[3].style.backgroundColor = 'green'
+//for(var i=0; i < items.length; i++)
+//{ items[i].style.fontweight = 'bold';}
+
+// Get ELEMENTSBYTAGNAME
+
+var li= document.getElementsByTagName('li');
+li[3].style.backgroundColor = 'green'
+for(var i=0; i < li.length; i++)
 {
- items[i].style.fontweight = 'bold';
+ li[i].style.fontweight = 'bold';
 }
